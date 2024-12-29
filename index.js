bd1b96bee79fe515dca843e3d5d5c59cef50744c
its-samarth <samarthkasma21@gmail.com>
2024-12-29 13:08:41 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+// Basic route
+app.get('/hello', (req, res) => {
+  res.send('Hello from backend');
+});
+
+app.listen(PORT, () => {
+  console.log(`Backend server running at http://localhost:${PORT}`);
+});
+
+// Export the app as a Lambda handler
+module.exports.handler = serverless(app);
