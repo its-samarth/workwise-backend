8ee37cc2a7adb891c016800ace6bf0cd451d5551
its-samarth <samarthkasma21@gmail.com>
2024-12-29 15:37:42 +0530

    configured cicd with docker ec2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const serverless = require('serverless-http');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
